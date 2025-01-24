9608d3cb7b403649a69fbfb3e080e329563e1c81
Subasish Mula <subasishmula@gmail.com>
2025-01-25 01:01:48 +0530

    latest update

---
+++ hello.py
@@ -0,0 +1,27 @@
+print("Hello Python")
+a = 200 # Integer
+Number = 123.12 #float
+name = "subasish" #String
+is_active = True #boolean
+numbers = [10,20,30,40,50,67] #list sequence of order items
+
+a = 5
+b = 2
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+# name = input("Enter your name : ")
+# print(f"Hello, {name}")
+# print("Hello " + name + " ")
+
+
+# print(type(is_active))
+# print(numbers)
+# print(is_active)
+# print(name)
+# print(Number)
+# print(a)
